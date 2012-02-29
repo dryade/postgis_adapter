26aa4344d6032cc4f89adc559fc1ac0585dc92a1
Alban Peignier <alban.peignier@dryade.net>
2012-02-29 17:04:19 +0100

    Remove explicit loading of PostgresqlAdapter

--- lib/postgis_adapter/railtie.rb
+++ lib/postgis_adapter/railtie.rb
@@ -1,10 +1,6 @@
 module PostgisAdapter
   class Railtie < Rails::Railtie
     initializer "postgis_adapter", :after => "active_record.initialize_database" do
-      unless defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-        require 'active_record/connection_adapters/postgresql_adapter'
-      end
-
       require 'postgis_adapter/init'
     end
   end
