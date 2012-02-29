b20569f8ff8eb402f86a55dc037d18ee7ff395b1
Alban Peignier <alban.peignier@dryade.net>
2012-02-29 12:10:09 +0100

    (Try to) init PostgistAdapter after active_record.initialize_database

--- lib/postgis_adapter/railtie.rb
+++ lib/postgis_adapter/railtie.rb
@@ -1,7 +1,11 @@
 module PostgisAdapter
   class Railtie < Rails::Railtie
-    initializer "postgis adapter" do
-      require "postgis_adapter"
+    initializer "postgis_adapter", :after => "active_record.initialize_database" do
+      unless defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+        require 'active_record/connection_adapters/postgresql_adapter'
+      end
+
+      require 'postgis_adapter/init'
     end
   end
 end
